23e820763fdd9eb47326613d45b13f84ed6baaae
anand21-hue <nandyalaanand3@gmail.com>
2026-01-19 17:57:31 +0530

    Add day 7 dictionary program

---
+++ studentdetails.py
@@ -0,0 +1,8 @@
+student={
+    "name":"Anand",
+    "age":  22,
+    "course":"statistics",
+    "marks":89}
+print("student details:")
+for key,values in student.items():
+    print(key,":",values)
